18f6a0ab1f020b998bdda756be7443685ac7b8c7
Tht-worada <thitibell123@gmail.com>
2026-01-17 12:30:41 +0700

    Thitiworada Main.java
    เลือกใช้ arraylist แทน array เพราะจะได้ใช้ collection หา min max ได้เลย ไม่ต้องเขียนเอง
    
    ปัญหาหลักคือ ยังทำให้แสดงผลมีจำนวนทศนิยมแบบอาจารย์ไม่ได้ ถ้าจะใช้ %.17f มันก็จะกลายเป็นบังคับว่ามี 17 ทุกตัว ซึ่งจากตัวอย่างไม่ใช่

--- ProjectQ2/src/Main.java
+++ ProjectQ2/src/Main.java
@@ -1,34 +1,40 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Collections;
 
 public class Main {
-    public static void main(String[] args){
-
-        Scanner sc = new Scanner(System.in);
-
-        Double[] x = new Double[4];
-        Double[] y = new Double[4];
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<Double> allx = new ArrayList<>();
+        List<Double> ally = new ArrayList<>();
+        String[] pointCount = {"First ", "Second ", "Third ", "Fourth "};
+        Double sumx = 0.0, sumy = 0.0, sumDis = 0.0;
 
         for (int i = 0 ; i < 4 ; i++) {
-            System.out.print(i + 1 + "(x,y):");
-            x[i] = sc.nextDouble();
-            y[i] = sc.nextDouble();
+            System.out.print(pointCount[i]+ "point: ");
+            allx.add(scanner.nextDouble());
+            ally.add(scanner.nextDouble());
+            sumx += allx.get(i);
+            sumy += ally.get(i);
         }
 
-        double SumX = 0;
-        double SumY = 0;
+        Double centroidx = sumx/4.0;
+        Double centroidy = sumy/4.0;
 
-        for (int i = 0; i < 4 ; i++){
-            SumX += x[i];
-            SumY += y[i];
+        List<Double> distance = new ArrayList<>();
+        for (int i = 0 ; i < 4 ; i++) {
+            distance.add(Math.sqrt(Math.pow(Math.abs(centroidx-allx.get(i)),2)+
+                    Math.pow(Math.abs(centroidy-ally.get(i)),2)));
+            sumDis += distance.get(i);
         }
-        System.out.println("The centroid is: " + "("+ SumX/4 + "," +SumY/4 + ")" );
 
-        double distance = Math.sqrt(Math.pow(SumX, 2) + Math.pow(SumY, 2));
-        System.out.println(distance);
+        Double minDis = Collections.min(distance);
+        Double maxDis = Collections.max(distance);
 
-
-
+        System.out.printf("The centroid is (%f, %f).\n", centroidx, centroidy);
+        System.out.printf("Sum of distance is %f.\n", sumDis);
+        System.out.printf("Shortest distance is %f.\nLongest distance is %f.",
+                minDis, maxDis);
     }
 }
-
-
